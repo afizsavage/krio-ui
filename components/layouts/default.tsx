3b767901819383be5bc93402aee9665135ec691a
Afiz Savage <savageafiz9@gmail.com>
2023-02-09 01:20:54 +0000

    define default-layout using grid

--- components/layouts/default.tsx
+++ components/layouts/default.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
 import Navbar from '../generic/navbar';
+import { DefaultLayoutProps } from '../../@types';
 
-type DefaultLayoutProps = {
-  children?: React.ReactNode;
-};
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={3}>
       <Grid item xs={12}>
         <Navbar />
+      </Grid>
+      <Grid sx={{ marginLeft: 3 }} item xs>
+        <Item>
+          <span>here</span>
+        </Item>
+      </Grid>
+      <Grid item xs={5}>
         {children}
+      </Grid>
+      <Grid sx={{ marginRight: 3 }} item xs>
+        <Item>
+          <span>here</span>
+        </Item>
       </Grid>
     </Grid>
   );
